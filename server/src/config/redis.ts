1a912779fea73a8d553d92f1144483c42db03391
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2025-07-23 18:52:07 +0530

    update redis config

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -3,6 +3,6 @@ import { RedisOptions } from "ioredis";
 export const redisConnection: RedisOptions = {
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD ,
-  tls: {}
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null,
 };
