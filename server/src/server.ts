62e1588af49272e1e86797f40d9bbf48964751c9
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2025-07-23 18:58:08 +0530

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,9 +5,7 @@ import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 
-const start = async () => {
+app.listen(PORT, async() => {
   await connectDB();
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-};
-
-start();
+  console.log("your website is served on http://localhost:"+PORT);
+});
