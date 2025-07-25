3cc5ca0948e842b186f6a7c23029f74aed0886de
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2025-07-25 15:12:17 +0530

    fix chore

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async() => {
-  await connectDB();
+connectDB();
+app.listen(PORT, () => {
   console.log("your website is served on http://localhost:"+PORT);
 });
